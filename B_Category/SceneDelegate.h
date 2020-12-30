3b7c8f0ab56bcd8b66477b3c87e1e02f95240604
fansen <18037860536@163.com>
2020-12-30 11:05:22 +0800

    Initial Commit

---
+++ B_Category/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  B_Category
+//
+//  Created by fansen on 2020/12/30.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ B_Category/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  B_Category
+//
+//  Created by fansen on 2020/12/30.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
